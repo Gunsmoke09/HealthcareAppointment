557ccbb5f1a6261489d012b3c4e8b03795197f5b
Gunsmoke <anant.enc@gmail.com>
2025-08-12 00:25:37 +1000

    changes to run locally

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5001", // local
-  baseURL: 'http://3.107.177.43:5001', // live
+  baseURL: 'http://54.252.246.51:5001', // live
   headers: { "Content-Type": "application/json" },
 });
 
