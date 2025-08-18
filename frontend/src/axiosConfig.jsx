f4b2fc276c9e5ae8670a424d7cc56b2f4f823292
Gunsmoke <anant.enc@gmail.com>
2025-08-18 13:48:03 +1000

    runtime

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,11 +2,11 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5001",
-  baseURL: 'http://3.25.239.91', // live
+  baseURL: 'http://3.24.124.30:5001', // live
   headers: { "Content-Type": "application/json" },
 });
 
 export default axiosInstance;
 
 
-// Changes for Final Workflow Run Test Push
+// Changes for Final Workflow Run Test 
